fdf1ac88329a3f01076ee37e259919e1f65bdcbb
Edwin Jakobs <edwin@rndr.studio>
2018-11-25 16:56:07 +0100

    Added box, plane and sphere mesh generators

---
+++ orx-mesh-generators/src/main/kotlin/Plane.kt
@@ -0,0 +1,72 @@
+package org.openrndr.extras.meshgenerators
+
+import org.openrndr.draw.VertexBuffer
+import org.openrndr.math.Vector2
+import org.openrndr.math.Vector3
+
+fun planeMesh(center: Vector3,
+              right: Vector3,
+              forward: Vector3,
+              up: Vector3 = forward.cross(right).normalized,
+              width: Double = 1.0, height: Double = 1.0,
+              widthSegments: Int = 1, heightSegments: Int = 2): VertexBuffer {
+
+    val vertexCount = (widthSegments * heightSegments) * 6
+    val vb = meshVertexBuffer(vertexCount)
+    vb.put {
+        generatePlane(center, forward, right, up,
+                width, height, widthSegments, heightSegments, bufferWriter(this))
+    }
+    return vb
+}
+
+/**
+ * generates a finite plane with its center at (0,0,0) and spanning the xz-plane
+ */
+fun groundPlaneMesh(width: Double = 1.0,
+                    height: Double = 1.0,
+                    widthSegments: Int = 1,
+                    heightSegments: Int): VertexBuffer {
+    return planeMesh(Vector3.ZERO, Vector3.UNIT_X, Vector3.UNIT_Z, Vector3.UNIT_Y,
+            width, height, widthSegments, heightSegments)
+}
+
+fun generatePlane(center: Vector3,
+                  right: Vector3,
+                  forward: Vector3,
+                  up: Vector3 = forward.cross(right).normalized,
+
+                  width: Double = 1.0, height: Double = 1.0,
+                  widthSegments: Int = 1, heightSegments: Int = 2,
+                  writer: VertexWriter) {
+
+    val forwardStep = forward.normalized * (height / heightSegments)
+    val rightStep = right.normalized * (width / widthSegments)
+
+    val corner = center - forward.normalized *  (height*0.5) - right.normalized * (width * 0.5)
+
+    val step = Vector2(1.0 / width, 1.0 / height)
+
+    for (v in 0 until heightSegments) {
+        for (u in 0 until widthSegments) {
+
+            val uv00 = Vector2(u + 0.0, v + 0.0) * step
+            val uv01 = Vector2(u + 0.0, v + 0.1) * step
+            val uv10 = Vector2(u + 1.0, v + 0.0) * step
+            val uv11 = Vector2(u + 1.0, v + 0.1) * step
+
+            val c00 = corner + forwardStep * v.toDouble() + rightStep * u.toDouble()
+            val c01 = corner + forwardStep * (v + 1).toDouble() + rightStep * u.toDouble()
+            val c10 = corner + forwardStep * v.toDouble() + rightStep * (u + 1).toDouble()
+            val c11 = corner + forwardStep * (v + 1).toDouble() + rightStep * (u + 1).toDouble()
+
+            writer(c00, up, uv00)
+            writer(c10, up, uv10)
+            writer(c11, up, uv11)
+
+            writer(c11, up, uv11)
+            writer(c01, up, uv01)
+            writer(c00, up, uv00)
+        }
+    }
+}
