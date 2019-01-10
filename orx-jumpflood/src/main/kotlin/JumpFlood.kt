010f6bf1b072f6244452dddd062678f3da8ff4e1
Edwin Jakobs <edwin@rndr.studio>
2019-01-10 12:48:52 +0100

    Bumped to 0.0.17

--- orx-jumpflood/src/main/kotlin/JumpFlood.kt
+++ orx-jumpflood/src/main/kotlin/JumpFlood.kt
@@ -2,7 +2,7 @@ package org.openrndr.extra.jumpfill
 
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.*
-import org.openrndr.filter.filterShaderFromUrl
+
 import org.openrndr.math.Matrix44
 import org.openrndr.math.Vector2
 import org.openrndr.resourceUrl
