3231f535a45cfc2fe65c81144663f433e76367f5
Edwin Jakobs <edwin@rndr.studio>
2019-09-24 18:23:57 +0200

    Replace trySetAccessible with direct version (did not compile on JDK 8)

--- orx-gradient-descent/src/main/kotlin/DataBinding.kt
+++ orx-gradient-descent/src/main/kotlin/DataBinding.kt
@@ -12,24 +12,24 @@ fun <T : Any> modelToArray(model: T): DoubleArray {
     model::class.java.declaredFields.forEach {
         when {
             it.type == Double::class.java -> {
-                it.trySetAccessible()
+                it.isAccessible = true
                 doubles.add(it.getDouble(model))
             }
             it.type == Vector2::class.java -> {
-                it.trySetAccessible()
+                it.isAccessible = true
                 val v2 = it.get(model) as Vector2
                 doubles.add(v2.x)
                 doubles.add(v2.y)
             }
             it.type == Vector3::class.java -> {
-                it.trySetAccessible()
+                it.isAccessible = true
                 val v3 = it.get(model) as Vector3
                 doubles.add(v3.x)
                 doubles.add(v3.y)
                 doubles.add(v3.z)
             }
             it.type == Vector4::class.java -> {
-                it.trySetAccessible()
+                it.isAccessible = true
                 val v4 = it.get(model) as Vector4
                 doubles.add(v4.x)
                 doubles.add(v4.y)
@@ -49,22 +49,22 @@ fun <T : Any> arrayToModel(data: DoubleArray, model: T) {
     model::class.java.declaredFields.forEach {
         when {
             it.type == Double::class.java -> {
-                it.trySetAccessible()
+                it.isAccessible = true
                 it.setDouble(model, data[index])
                 index++
             }
             it.type == Vector2::class.java -> {
-                it.trySetAccessible()
+                it.isAccessible = true
                 it.set(model, Vector2(data[index], data[index+1]))
                 index+=2
             }
             it.type == Vector3::class.java -> {
-                it.trySetAccessible()
+                it.isAccessible = true
                 it.set(model, Vector3(data[index], data[index+1],data[index+2]))
                 index+=3
             }
             it.type == Vector4::class.java -> {
-                it.trySetAccessible()
+                it.isAccessible = true
                 it.set(model, Vector4(data[index], data[index+1],data[index+2],data[index+3]))
                 index+=3
             }
