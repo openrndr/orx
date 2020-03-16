a198b009681564bdbf01d2a7388aeb76c21408e6
Edwin Jakobs <edwin@rndr.studio>
2020-03-16 23:51:30 +0100

    Add parameter annotations to orx-shade-styles

--- orx-shade-styles/src/main/kotlin/AngularGradient.kt
+++ orx-shade-styles/src/main/kotlin/AngularGradient.kt
@@ -2,26 +2,20 @@ package org.openrndr.extra.shadestyles
 
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.ShadeStyle
-import org.openrndr.draw.shadeStyle
 import org.openrndr.extra.parameters.ColorParameter
 import org.openrndr.extra.parameters.Description
-import org.openrndr.extra.parameters.DoubleParameter
 import org.openrndr.math.Vector2
 
-@Description("Linear gradient")
-class LinearGradient(
-        color0: ColorRGBa,
-        color1: ColorRGBa,
-        offset: Vector2,
-        rotation: Double) : ShadeStyle() {
-
+@Description("Angular gradient")
+class AngularGradient(color0: ColorRGBa, color1: ColorRGBa, offset: Vector2 = Vector2.ZERO, rotation: Double = 0.0) : ShadeStyle() {
     @ColorParameter("start color", order = 0)
-    var color0: ColorRGBa by Parameter()
+    var color0 : ColorRGBa by Parameter()
     @ColorParameter("end color", order = 1)
-    var color1: ColorRGBa by Parameter()
-    var offset: Vector2 by Parameter()
-    @DoubleParameter("rotation", -180.0, 180.0, order = 2)
-    var rotation: Double by Parameter()
+    var color1 : ColorRGBa by Parameter()
+    @ColorParameter("offset", order = 2)
+    var offset : Vector2 by Parameter()
+    @ColorParameter("rotation", order = 3)
+    var rotation : Double by Parameter()
 
     init {
         this.color0 = color0
@@ -30,15 +24,17 @@ class LinearGradient(
         this.rotation = rotation
 
         fragmentTransform = """
-            vec2 coord = (c_boundsPosition.xy - vec2(0.5) + p_offset);
+            vec2 coord = (c_boundsPosition.xy - vec2(0.5) + p_offset/2.0) * 2.0;
+            
             float cr = cos(radians(p_rotation));
             float sr = sin(radians(p_rotation));
             mat2 rm = mat2(cr, -sr, sr, cr);
-            float f = clamp((rm * coord).y + 0.5, 0.0, 1.0);            
-                
+            vec2 rc = rm * coord;
+            float f = (atan(rc.y, rc.x) + 3.1415926536) / (2.0 * 3.1415926536);            
+            
             vec4 color0 = p_color0 * vec4(p_color0.aaa, 1.0);
             vec4 color1 = p_color1 * vec4(p_color1.aaa, 1.0);
-
+                                    
             vec4 gradient = color0 * (1.0-f) + color1 * f;
             
             vec4 fn = vec4(x_fill.rgb, 1.0) * x_fill.a;            
@@ -51,8 +47,6 @@ class LinearGradient(
     }
 }
 
-fun linearGradient(color0: ColorRGBa, color1: ColorRGBa, offset: Vector2 = Vector2.ZERO, rotation: Double = 0.0) =
-        LinearGradient(color0, color1, offset, rotation)
-
-
-
+fun angularGradient(color0: ColorRGBa, color1: ColorRGBa, offset: Vector2 = Vector2.ZERO, rotation: Double = 0.0): ShadeStyle {
+    return AngularGradient(color0, color1, offset, rotation)
+}
--- orx-shade-styles/src/main/kotlin/HalfAngularGradient.kt
+++ orx-shade-styles/src/main/kotlin/HalfAngularGradient.kt
@@ -2,19 +2,11 @@ package org.openrndr.extra.shadestyles
 
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.ShadeStyle
-import org.openrndr.draw.shadeStyle
 import org.openrndr.extra.parameters.ColorParameter
-import org.openrndr.extra.parameters.Description
 import org.openrndr.extra.parameters.DoubleParameter
 import org.openrndr.math.Vector2
 
-@Description("Linear gradient")
-class LinearGradient(
-        color0: ColorRGBa,
-        color1: ColorRGBa,
-        offset: Vector2,
-        rotation: Double) : ShadeStyle() {
-
+class HalfAngularGradient(color0: ColorRGBa, color1: ColorRGBa, offset: Vector2 = Vector2.ZERO, rotation: Double = 0.0) : ShadeStyle() {
     @ColorParameter("start color", order = 0)
     var color0: ColorRGBa by Parameter()
     @ColorParameter("end color", order = 1)
@@ -22,37 +14,42 @@ class LinearGradient(
     var offset: Vector2 by Parameter()
     @DoubleParameter("rotation", -180.0, 180.0, order = 2)
     var rotation: Double by Parameter()
+    @DoubleParameter("length", 0.0, 10.0, order = 3)
+    var length: Double by Parameter()
 
     init {
         this.color0 = color0
         this.color1 = color1
         this.offset = offset
         this.rotation = rotation
+        this.length = length
 
         fragmentTransform = """
-            vec2 coord = (c_boundsPosition.xy - vec2(0.5) + p_offset);
+            vec2 coord = (c_boundsPosition.xy - vec2(0.5) + p_offset/2.0) * 2.0;
+            
             float cr = cos(radians(p_rotation));
             float sr = sin(radians(p_rotation));
             mat2 rm = mat2(cr, -sr, sr, cr);
-            float f = clamp((rm * coord).y + 0.5, 0.0, 1.0);            
-                
+            vec2 rc = rm * coord;
+            float f = abs(atan(rc.y, rc.x)) / (3.1415926536);
+            
+            //float f = abs(atan(rc.y/rc.x))  / (3.1415926536/2.0);
+            //float f = (atan(rc.y/rc.x) + 3.1415926536/2.0) / (3.1415926536);            
+                   
             vec4 color0 = p_color0 * vec4(p_color0.aaa, 1.0);
             vec4 color1 = p_color1 * vec4(p_color1.aaa, 1.0);
-
+                       
             vec4 gradient = color0 * (1.0-f) + color1 * f;
-            
             vec4 fn = vec4(x_fill.rgb, 1.0) * x_fill.a;            
-            
             x_fill = fn * gradient;
             if (x_fill.a !=0) {
                 x_fill.rgb /= x_fill.a;
-            }
-        """
+            }        
+    }
+    """
     }
 }
 
-fun linearGradient(color0: ColorRGBa, color1: ColorRGBa, offset: Vector2 = Vector2.ZERO, rotation: Double = 0.0) =
-        LinearGradient(color0, color1, offset, rotation)
-
-
-
+fun halfAngularGradient(color0: ColorRGBa, color1: ColorRGBa, offset: Vector2 = Vector2.ZERO, rotation: Double = 0.0): ShadeStyle {
+    return HalfAngularGradient(color0, color1, offset, rotation)
+}
