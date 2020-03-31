7ed143cd436a1bb9dea9f354bd497b7bd7c0a38e
Abe Pazos <abe@hamoid.com>
2020-03-31 15:40:57 +0200

    TODO()s in matcher are triggered. Remove

--- orx-panel/src/main/kotlin/org/openrndr/panel/style/Matcher.kt
+++ orx-panel/src/main/kotlin/org/openrndr/panel/style/Matcher.kt
@@ -48,8 +48,8 @@ class Matcher {
                 Combinator.LATER_SIBLING -> if (result == MatchingResult.RESTART_FROM_CLOSEST_DESCENDANT) {
                     return result
                 }
-                Combinator.DESCENDANT -> TODO()
-                null -> TODO()
+                //Combinator.DESCENDANT -> TODO()
+                //null -> TODO()
             }
         }
     }
