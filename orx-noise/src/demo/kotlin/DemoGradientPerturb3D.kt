36f5d5b96d21b30abd90ad95e9544afcd4429495
Edwin Jakobs <edwin@rndr.studio>
2020-04-06 22:06:40 +0200

    Add gradientPerturb (3d version only)

---
+++ orx-noise/src/demo/kotlin/DemoGradientPerturb3D.kt
@@ -0,0 +1,27 @@
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.colorBuffer
+import org.openrndr.extra.noise.gradientPerturbFractal
+import org.openrndr.extra.noise.simplex
+import org.openrndr.math.Vector3
+import kotlin.math.absoluteValue
+
+fun main() {
+    application {
+        program {
+            val cb = colorBuffer(width, height)
+            val shad = cb.shadow
+            extend {
+                for (y in 0 until height) {
+                    for (x in 0 until width) {
+                        val p = gradientPerturbFractal(300, frequency = 0.8, position = Vector3(x/320.0,y/240.0, seconds))
+                        val d = simplex(300, p.x, p.y, p.z).absoluteValue
+                        shad[x, y] = ColorRGBa(d, d, d, 1.0)
+                    }
+                }
+                shad.upload()
+                drawer.image(cb)
+            }
+        }
+    }
+}
---
+++ orx-noise/src/main/kotlin/GradientPerturb.kt
@@ -0,0 +1,77 @@
+package org.openrndr.extra.noise
+
+import org.openrndr.math.Vector3
+import org.openrndr.math.mix
+
+fun gradientPerturbFractal(seed: Int, amplitude: Double = 1.0, frequency: Double = 2.0,
+                           lacunarity: Double = 2.0, gain: Double = 0.5,
+                           octaves: Int = 4, position: Vector3, interpolator: (Double) -> Double = ::quintic): Vector3 {
+
+    var seed_ = seed
+    var amp_ = amplitude
+    var frequency_ = frequency
+
+    var p = gradientPerturb(seed_, amp_, frequency_, position, interpolator)
+    for (i in 0 until octaves) {
+        frequency_ *= lacunarity
+        amp_ *= gain
+        seed_++
+        p = gradientPerturb(seed_, amp_, frequency_, p, interpolator)
+    }
+    return p
+}
+
+fun gradientPerturb(seed: Int, amplitude: Double, frequency: Double, position: Vector3, interpolator: (Double) -> Double = ::quintic): Vector3 {
+    val xf = position.x * frequency
+    val yf = position.y * frequency
+    val zf = position.z * frequency
+
+    val x0 = xf.fastFloor()
+    val y0 = yf.fastFloor()
+    val z0 = zf.fastFloor()
+    val x1 = x0 + 1
+    val y1 = y0 + 1
+    val z1 = z0 + 1
+
+    val xs = interpolator(xf - x0)
+    val ys = interpolator(yf - y0)
+    val zs = interpolator(zf - z0)
+
+    var vec0 = Cell3D[hash3D(seed, x0, y0, z0) and 0xff]
+    var vec1 = Cell3D[hash3D(seed, x1, y0, z0) and 0xff]
+
+    var lx0x = mix(vec0.x, vec1.x, xs)
+    var ly0x = mix(vec0.y, vec1.y, xs)
+    var lz0x = mix(vec0.z, vec1.z, xs)
+
+    vec0 = Cell3D[hash3D(seed, x0, y1, z0) and 0xff]
+    vec1 = Cell3D[hash3D(seed, x1, y1, z0) and 0xff]
+
+    var lx1x = mix(vec0.x, vec1.x, xs)
+    var ly1x = mix(vec0.y, vec1.y, xs)
+    var lz1x = mix(vec0.z, vec1.z, xs)
+
+    val lx0y = mix(lx0x, lx1x, ys)
+    val ly0y = mix(ly0x, ly1x, ys)
+    val lz0y = mix(lz0x, lz1x, ys)
+
+    vec0 = Cell3D[hash3D(seed, x0, y0, z1) and 0xff]
+    vec1 = Cell3D[hash3D(seed, x1, y0, z1) and 0xff]
+
+    lx0x = mix(vec0.x, vec1.x, xs)
+    ly0x = mix(vec0.y, vec1.y, xs)
+    lz0x = mix(vec0.z, vec1.z, xs)
+
+    vec0 = Cell3D[hash3D(seed, x0, y1, z1) and 0xff]
+    vec1 = Cell3D[hash3D(seed, x1, y1, z1) and 0xff]
+
+    lx1x = mix(vec0.x, vec1.x, xs)
+    ly1x = mix(vec0.y, vec1.y, xs)
+    lz1x = mix(vec0.z, vec1.z, xs)
+
+    return position + Vector3(
+            mix(lx0y, mix(lx0x, lx1x, ys), zs),
+            mix(ly0y, mix(ly0x, ly1x, ys), zs),
+            mix(lz0y, mix(lz0x, lz1x, ys), zs)
+    ) * amplitude
+}
