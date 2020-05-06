75fcb6d5affad11a0cfb064854703cda37b809cb
Edwin Jakobs <edwin@rndr.studio>
2020-05-06 10:00:22 +0200

    Remove package from script generator

--- orx-olive/src/main/kotlin/ScriptGenerator.kt
+++ orx-olive/src/main/kotlin/ScriptGenerator.kt
@@ -5,13 +5,12 @@ import org.openrndr.extra.kotlinparser.ProgramSource
 inline fun <reified T> generateScript(programSource: ProgramSource): String {
     val script = """
         
-@file:Suppress("UNUSED_LAMBDA_EXPRESSION")
-${programSource.packageName?:""}
+//${programSource.packageName?:""}
 
 import org.openrndr.extra.olive.OliveProgram
 ${programSource.imports}        
 
-{ program: ${T::class.simpleName} ->
+{ program: ${T::class.qualifiedName} ->
         program.apply {
             ${programSource.programLambda}                    
         }
