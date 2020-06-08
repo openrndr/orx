8f9e9c9d9ed6a9f856bb1127d900ceb50ed94be5
Edwin Jakobs <edwin@rndr.studio>
2020-06-08 21:56:36 +0200

    Add demo / tester for 16 bit png loading

---
+++ openrndr-demos/src/demo/kotlin/Demo16BitPng.kt
@@ -0,0 +1,16 @@
+
+import org.openrndr.application
+import org.openrndr.draw.loadImage
+
+fun main() {
+    application {
+        program {
+            val image16 = loadImage("demo-data/images/16-bit.png")
+            val image8 = loadImage("demo-data/images/image-001.png")
+            extend {
+                drawer.image(image16)
+                drawer.image(image8, 320.0, 0.0)
+            }
+        }
+    }
+}
