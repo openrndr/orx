b9ba834c7d32e8e795de4d2d2ab76fc1f8b8ad50
BoydRotgans <boyd@rndr.studio>
2020-06-10 11:47:43 +0200

    Update description

--- orx-runway/src/demo/kotlin/PoseNet01.kt
+++ orx-runway/src/demo/kotlin/PoseNet01.kt
@@ -1,14 +1,13 @@
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
-import org.openrndr.draw.ColorBuffer
 import org.openrndr.draw.isolatedWithTarget
 import org.openrndr.draw.loadImage
 import org.openrndr.draw.renderTarget
 import org.openrndr.extra.runway.*
 
 /**
- * This demonstrates the body estimation model of DensePose
- * This example requires a `runway/DensePose` model active in Runway.
+ * This demonstrates the body estimation model of PoseNet
+ * This example requires a `runway/PoseNet` model active in Runway.
  */
 fun main() = application {
     configure {
