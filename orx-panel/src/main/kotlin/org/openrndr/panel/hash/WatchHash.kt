7b70ae4c9069315dc97576a3346701be4fff81dd
Edwin Jakobs <edwin@rndr.studio>
2020-08-18 10:02:55 +0200

    [orx-panel] Fix watchHash for Kotlin 1.4

--- orx-panel/src/main/kotlin/org/openrndr/panel/hash/WatchHash.kt
+++ orx-panel/src/main/kotlin/org/openrndr/panel/hash/WatchHash.kt
@@ -7,7 +7,7 @@ import kotlin.reflect.full.declaredMemberProperties
 fun watchHash(toHash: Any): Int {
     var hash = 0
     for (property in toHash::class.declaredMemberProperties) {
-        val v = ((property.getter) as KProperty1<Any, Any>).invoke(toHash)
+        val v = ((property as KProperty1<Any, Any?>).getter).invoke(toHash)
         if (v is KProperty0<*>) {
             val pv = v.get()
             hash = 31 * hash + (pv?.hashCode() ?: 0)
