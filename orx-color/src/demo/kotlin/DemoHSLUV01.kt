982e09a819dd0c4af58ebf5955638021adcf1dee
Abe Pazos <abe@hamoid.com>
2020-08-22 15:58:04 +0200

    Add text labels to DemoHSLUV01

--- orx-color/src/demo/kotlin/DemoHSLUV01.kt
+++ orx-color/src/demo/kotlin/DemoHSLUV01.kt
@@ -2,7 +2,9 @@
 
 import org.openrndr.application
 import org.openrndr.color.ColorHSLa
+import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.isolated
+import org.openrndr.draw.loadFont
 import org.openrndr.extensions.SingleScreenshot
 import org.openrndr.extras.color.spaces.ColorHSLUVa
 import org.openrndr.math.Vector2
@@ -17,6 +19,7 @@ fun main() {
                     this.outputFile = System.getProperty("screenshotPath")
                 }
             }
+            val font = loadFont("demo-data/fonts/IBMPlexMono-Regular.ttf", 24.0)
             extend {
                 drawer.stroke = null
                 for (a in 0 until 360 step 12) {
@@ -29,9 +32,15 @@ fun main() {
                         rectangle(Rectangle(pos * 1.2, 40.0, 300.0))
 
                         fill = ColorHSLa(a * 1.0, 1.0, 0.5).toRGBa()
-                        rectangle(Rectangle.fromCenter(pos,30.0, 60.0))
+                        rectangle(Rectangle.fromCenter(pos, 30.0, 60.0))
                     }
                 }
+                drawer.fontMap = font
+                drawer.fill = ColorRGBa.WHITE
+                drawer.text("HSLa", width * 0.48, height * 0.73)
+                drawer.text("HSLUVa", width * 0.8, height * 0.52)
+                drawer.text("hue: 0 to 360, saturation: 1.0, " +
+                        "lightness: 0.5", 50.0, 460.0)
             }
         }
     }
