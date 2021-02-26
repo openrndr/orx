5b0a6f37e009b1b9b6ad2f72c743c8b55dd7a8ec
Ricardo Matias <ricardo.matias0@gmail.com>
2021-02-26 17:51:03 +0100

    [orx-quadtree] add quadtree module (#174)

---
+++ orx-quadtree/README.md
@@ -0,0 +1,22 @@
+# orx-quadtree
+
+An extension for creating a [Quadtree](https://en.wikipedia.org/wiki/Quadtree) for points. A quadtree is a spatial
+partioning tree structure meant to provide fast spatial queries such as nearest points within a range.
+
+## Example
+
+```kotlin
+val box = Rectangle.fromCenter(Vector2(400.0), 750.0)
+
+val quadTree = Quadtree<Vector2>(box) { it }
+
+for (point in points) {
+    quadTree.insert(point)
+}
+
+val nearestQuery = quadTree.nearest(points[4], 20.0)
+```
+
+### Author
+
+Ricardo Matias / [@ricardomatias](https://github.com/ricardomatias)
---
+++ orx-quadtree/src/demo/kotlin/DemoQuadTree01.kt
@@ -0,0 +1,55 @@
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.rectangleBatch
+import org.openrndr.extensions.SingleScreenshot
+import org.openrndr.extra.noise.Random
+import org.openrndr.extra.noise.gaussian
+import org.openrndr.math.Vector2
+import org.openrndr.shape.Rectangle
+import quadtree.Quadtree
+
+fun main() {
+    application {
+        configure {
+            width = 800
+            height = 800
+            title = "QuadTree"
+        }
+        program {
+            if (System.getProperty("takeScreenshot") == "true") {
+                extend(SingleScreenshot()) {
+                    this.outputFile = System.getProperty("screenshotPath")
+                }
+            }
+
+            val box = Rectangle.fromCenter(Vector2(400.0), 750.0)
+
+            val points = (0 until 1_000).map {
+                Vector2.gaussian(box.center, Vector2(95.0), Random.rnd)
+            }
+
+            val quadTree = Quadtree<Vector2>(box) { it }
+
+            for (point in points) {
+                quadTree.insert(point)
+            }
+
+            val batch = drawer.rectangleBatch {
+                this.fill = null
+                this.stroke = ColorRGBa.GRAY
+                this.strokeWeight = 0.5
+                quadTree.batch(this)
+            }
+
+            extend {
+                drawer.clear(ColorRGBa.BLACK)
+
+                drawer.rectangles(batch)
+
+                drawer.fill = ColorRGBa.PINK.opacify(0.7)
+                drawer.stroke = null
+                drawer.circles(points, 5.0)
+            }
+        }
+    }
+}
