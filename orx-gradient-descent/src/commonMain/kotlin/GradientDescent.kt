4f3834df7b59a4a76035e49d1a4465ef7d560a94
Edwin Jakobs <edwin@rndr.studio>
2021-06-29 15:26:59 +0200

    [orx-gradient-descent] convert to MPP

--- orx-gradient-descent/src/commonMain/kotlin/GradientDescent.kt
+++ orx-gradient-descent/src/commonMain/kotlin/GradientDescent.kt
@@ -175,12 +175,3 @@ fun minimize(_x0: DoubleArray,
     return MinimizationResult(x0, f0, g0, H1, iteration)
 }
 
-fun <T : Any> minimizeModel(model: T, endOnLineSearch: Boolean = false, tol: Double = 1e-8, maxIterations: Int = 1000, function: (T) -> Double) {
-    val doubles = modelToArray(model)
-    val weights = DoubleArray(doubles.size) { 1.0 }
-    val solution = minimize(doubles, weights, endOnLineSearch, tol, maxIterations) {
-        arrayToModel(it, model)
-        function(model)
-    }
-    arrayToModel(solution.solution, model)
-}
