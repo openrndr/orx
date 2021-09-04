9317f8dca905e8a511fbf3c87ee143a53882513d
Rein van der Woerd <reinvanderwoerd@me.com>
2021-09-04 18:35:53 +0200

    Converted orx-gui's build script to Kotlin

---
+++ orx-jvm/orx-gui/build.gradle.kts
@@ -0,0 +1,34 @@
+sourceSets {
+    val main by getting { }
+    val demo by creating {
+        java {
+            srcDir("src/demo/kotlin")
+            compileClasspath += main.getCompileClasspath()
+            runtimeClasspath += main.getRuntimeClasspath()
+        }
+    }
+}
+
+
+val openrndrVersion: String by rootProject.extra
+val openrndrOS: String by rootProject.extra
+val gsonVersion: String by rootProject.extra
+val kotlinVersion: String by rootProject.extra
+
+val demoImplementation by configurations.getting {}
+val demoRuntimeOnly by configurations.getting {}
+
+
+dependencies {
+    api(project(":orx-parameters"))
+    api(project(":orx-jvm:orx-panel"))
+
+    implementation("org.openrndr:openrndr-dialogs:$openrndrVersion")
+    implementation("com.google.code.gson:gson:$gsonVersion")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    demoImplementation("org.openrndr:openrndr-application:$openrndrVersion")
+    demoImplementation("org.openrndr:openrndr-extensions:$openrndrVersion")
+    demoRuntimeOnly("org.openrndr:openrndr-gl3:$openrndrVersion")
+    demoRuntimeOnly("org.openrndr:openrndr-gl3-natives-$openrndrOS:$openrndrVersion")
+    demoImplementation(sourceSets.getByName("main").output)
+}
