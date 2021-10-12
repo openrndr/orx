cec114076310db68098d7b700663a956168fcf56
Rein van der Woerd <reinvanderwoerd@me.com>
2021-10-12 16:22:36 +0200

    Converted orx-time-operator's build script to Kotlin

--- orx-time-operators/build.gradle.kts
+++ orx-time-operators/build.gradle.kts
@@ -1,20 +1,31 @@
 sourceSets {
-    demo {
+    val main by getting { }
+    val demo by creating {
         java {
-            srcDirs = ["src/demo/kotlin"]
+            srcDir("src/demo/kotlin")
             compileClasspath += main.getCompileClasspath()
             runtimeClasspath += main.getRuntimeClasspath()
         }
     }
 }
 
+val openrndrVersion: String by rootProject.extra
+val openrndrOS: String by rootProject.extra
+val demoImplementation by configurations.getting {}
+val demoRuntimeOnly by configurations.getting {}
+
+
+
 dependencies {
-    implementation project(":orx-parameters")
+    implementation(project(":orx-parameters"))
+
 
     demoImplementation(project(":orx-camera"))
     demoImplementation("org.openrndr:openrndr-application:$openrndrVersion")
     demoImplementation("org.openrndr:openrndr-extensions:$openrndrVersion")
     demoRuntimeOnly("org.openrndr:openrndr-gl3:$openrndrVersion")
     demoRuntimeOnly("org.openrndr:openrndr-gl3-natives-$openrndrOS:$openrndrVersion")
+
     demoImplementation(sourceSets.getByName("main").output)
+
 }
