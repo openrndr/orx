907525f71f38ee5b4e7f1e0f0e2f9bd905976f77
Yvee1 <stevenvdb@live.nl>
2021-10-28 13:48:03 +0200

    [orx-shapes] Add alpha shapes with holes using opernndr's Shape

--- orx-shapes/src/demo/kotlin/DemoHobbyCurve02.kt
+++ orx-shapes/src/demo/kotlin/DemoHobbyCurve02.kt
@@ -7,14 +7,14 @@ import kotlin.random.Random
 
 fun main() = application {
     program {
-        val points = List(20) {
+        val points = List(40) {
             Vector2(
                 Random.nextDouble(width*0.25, width*0.75),
                 Random.nextDouble(height*0.25, height*0.75)
             )
         }
         val alphaShape = AlphaShape(points)
-        val c = alphaShape.create()
+        val c = alphaShape.createContour()
         val hobby = hobbyCurve(c.segments.map { it.start }, closed=true)
         extend {
             drawer.fill = ColorRGBa.PINK
