a85bb181361a77e1130b893cfc4a72aaf29403d7
Edwin Jakobs <edwin@rndr.studio>
2021-11-28 23:27:16 +0100

    Switch to publish-plugin and fix pom info for orx-git-archiver-gradle

--- orx-jvm/orx-git-archiver-gradle/build.gradle.kts
+++ orx-jvm/orx-git-archiver-gradle/build.gradle.kts
@@ -15,3 +15,38 @@ gradlePlugin {
         }
     }
 }
+
+publishing {
+    afterEvaluate {
+        publications {
+            withType(MavenPublication::class) {
+                pom {
+                    name.set("gitarchiver tomarkdown")
+                    description.set("gitarchiver to markdown gradle plugin")
+                    url.set("https://openrndr.org")
+                    developers {
+                        developer {
+                            id.set("edwinjakobs")
+                            name.set("Edwin Jakobs")
+                            email.set("edwin@openrndr.org")
+                        }
+                    }
+
+                    licenses {
+                        license {
+                            name.set("BSD-2-Clause")
+                            url.set("https://github.com/openrndr/openrndr/blob/master/LICENSE")
+                            distribution.set("repo")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git@github.com:openrndr/orx.git")
+                        developerConnection.set("scm:git:ssh://github.com/openrndr/orx.git")
+                        url.set("https://github.com/openrndr/openrndr")
+                    }
+                }
+            }
+        }
+    }
+}
