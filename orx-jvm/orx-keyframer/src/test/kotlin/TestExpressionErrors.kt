b0e72b794555bc14a38ff6a24b5aac5065af3ad4
Edwin Jakobs <edwin@rndr.studio>
2022-07-28 19:10:28 +0200

    [orx-keyframer] Add support for backticked identifier names, replace spek tests

--- orx-jvm/orx-keyframer/src/test/kotlin/TestExpressionErrors.kt
+++ orx-jvm/orx-keyframer/src/test/kotlin/TestExpressionErrors.kt
@@ -3,55 +3,50 @@ import org.amshove.kluent.`with message`
 import org.amshove.kluent.invoking
 import org.openrndr.extra.keyframer.ExpressionException
 import org.openrndr.extra.keyframer.evaluateExpression
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
-import java.lang.IllegalStateException
+import kotlin.test.Test
 
-object TestExpressionErrors : Spek({
+class TestExpressionErrors {
 
-    describe("an expression with non-sensible writing") {
+    @Test
+    fun `an expression with non-sensible writing`() {
         val expression = ")("
-        it("should cause an exception to be thrown when evaluated") {
-            invoking {
-                evaluateExpression(expression)
-            } `should throw`  ExpressionException::class `with message` "parser error in expression: ')('; [line: 1, character: 0 , near: [@0,0:0=')',<21>,1:0] ]"
-        }
+        invoking {
+            evaluateExpression(expression)
+        } `should throw` ExpressionException::class `with message` "parser error in expression: ')('; [line: 1, character: 0 , near: [@0,0:0=')',<21>,1:0] ]"
+
     }
 
-    describe("an expression with equality instead of assign") {
+    @Test
+    fun `an expression with equality instead of assign`() {
         val expression = "a == 5"
-        it("should cause an exception to be thrown when evaluated") {
-            invoking {
-                evaluateExpression(expression)
-            }  `should throw` ExpressionException::class `with message` "parser error in expression: 'a == 5'; [line: 1, character: 3 , near: [@3,3:3='=',<19>,1:3] ]"
-        }
+        invoking {
+            evaluateExpression(expression)
+        } `should throw` ExpressionException::class `with message` "parser error in expression: 'a == 5'; [line: 1, character: 3 , near: [@3,3:3='=',<19>,1:3] ]"
+
     }
 
-    describe("an expression trying to reassign a number") {
+    @Test
+    fun `an expression trying to reassign a number`() {
         val expression = "3 = 5"
-        it("should cause an exception to be thrown when evaluated") {
-            invoking {
-               evaluateExpression(expression)
-            } `should throw` ExpressionException::class `with message` "parser error in expression: '3 = 5'; [line: 1, character: 2 , near: [@2,2:2='=',<19>,1:2] ]"
-        }
+        invoking {
+            evaluateExpression(expression)
+        } `should throw` ExpressionException::class `with message` "parser error in expression: '3 = 5'; [line: 1, character: 2 , near: [@2,2:2='=',<19>,1:2] ]"
     }
 
-    describe("an expression that uses non-existing functions") {
+    @Test
+    fun `an expression that uses non-existing functions`() {
         val expression = "notExisting(5)"
-        it("should cause an exception to be thrown when evaluated") {
-            invoking {
-                evaluateExpression(expression)
-            } `should throw` ExpressionException::class `with message` "error in evaluation of 'notExisting(5)': unresolved function: 'notExisting(x0)'"
-        }
+        invoking {
+            evaluateExpression(expression)
+        } `should throw` ExpressionException::class `with message` "error in evaluation of 'notExisting(5)': unresolved function: 'notExisting(x0)'"
+
     }
 
-    describe("an expression that uses non-existing variables") {
+    @Test
+    fun `an expression that uses non-existing variables`() {
         val expression = "notExisting + 4"
-        it("should cause an exception to be thrown when evaluated") {
-            invoking {
-                evaluateExpression(expression)
-            } `should throw` ExpressionException::class `with message` "error in evaluation of 'notExisting+4': unresolved variable: 'notExisting'"
-        }
+        invoking {
+            evaluateExpression(expression)
+        } `should throw` ExpressionException::class `with message` "error in evaluation of 'notExisting+4': unresolved variable: 'notExisting'"
     }
-
-})
+}
