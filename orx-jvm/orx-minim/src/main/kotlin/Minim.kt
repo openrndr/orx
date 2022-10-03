6a38307032bb7d6b6a6178c2fa0bbc4978b6afcf
Abe Pazos <abe@hamoid.com>
2022-10-03 16:09:24 +0200

    [orx-minim] Fix audio file loading, add demo

--- orx-jvm/orx-minim/src/main/kotlin/Minim.kt
+++ orx-jvm/orx-minim/src/main/kotlin/Minim.kt
@@ -3,10 +3,11 @@ package org.openrndr.extra.minim
 import ddf.minim.Minim
 import org.openrndr.Program
 import java.io.File
+import java.io.InputStream
 
 class MinimObject {
     fun sketchPath(fileName:String) = "./"
-    fun createInput(fileName: String) = File(fileName).inputStream()
+    fun createInput(fileName: String) = File(fileName).inputStream() as InputStream
 }
 
 fun Program.minim(): Minim  {
