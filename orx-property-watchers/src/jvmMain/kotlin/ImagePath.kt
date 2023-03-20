8e3e4bddb06a0d6aac3f3a45dbabf325ab1aadae
Edwin Jakobs <edwin@rndr.studio>
2023-03-20 14:13:26 +0100

    [orx-property-watchers] Add imageChangedEvent parameter

--- orx-property-watchers/src/jvmMain/kotlin/ImagePath.kt
+++ orx-property-watchers/src/jvmMain/kotlin/ImagePath.kt
@@ -2,6 +2,7 @@ package org.openrndr.extra.propertywatchers
 
 import org.openrndr.draw.ColorBuffer
 import org.openrndr.draw.loadImage
+import org.openrndr.events.Event
 import java.io.File
 import kotlin.reflect.KProperty0
 
@@ -11,8 +12,12 @@ import kotlin.reflect.KProperty0
  * @param imageTransform an optional image transform function
  * @since 0.4.3
  */
-fun watchingImagePath(pathProperty: KProperty0<String>, imageTransform: (ColorBuffer) -> ColorBuffer = { it }) =
-    watchingProperty(pathProperty, cleaner = { it.destroy() }) {
+fun watchingImagePath(
+    pathProperty: KProperty0<String>,
+    imageChangedEvent: Event<ColorBuffer>? = null,
+    imageTransform: (ColorBuffer) -> ColorBuffer = { it }
+) =
+    watchingProperty(pathProperty, imageChangedEvent, cleaner = { it.destroy() }) {
         val file = File(it)
         require(file.exists()) { "$it does not exist" }
         require(file.isFile) { "$it is not a file" }
