07f6244929bc626e0f184c61124d5dc8b67fffff
Abe Pazos <abe@hamoid.com>
2023-04-15 13:28:10 +0200

    Remove pressedButtons, now implemented via MouseTracker

--- orx-view-box/src/commonMain/kotlin/ViewBox.kt
+++ orx-view-box/src/commonMain/kotlin/ViewBox.kt
@@ -56,8 +56,10 @@ class ViewBox(
         override val moved = Event<MouseEvent>()
         override var position: Vector2 = -clientArea.corner
 
-        override val pressedButtons: MutableSet<MouseButton>
-            get() = TODO("Not yet implemented")
+        // Note: replaced by MouseTracker()
+        //override val pressedButtons: MutableSet<MouseButton>
+        //    get() = TODO("Not yet implemented")
+
         override val scrolled = Event<MouseEvent>()
     }
 
