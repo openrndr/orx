b3f694fbb7ff87a26736688df598e5a5f725830e
Edwin Jakobs <edwin@rndr.studio>
2023-12-07 16:53:32 +0100

    [orx-color] Add type aliases

---
+++ orx-color/src/commonMain/kotlin/spaces/TypeAliases.kt
@@ -0,0 +1,20 @@
+package org.openrndr.extra.color.spaces
+
+import org.openrndr.color.*
+
+typealias RGB = ColorRGBa
+typealias LAB = ColorLABa
+typealias LUV = ColorLUVa
+typealias HSL = ColorHSLa
+typealias HSV = ColorHSVa
+typealias HSLuv = ColorHSLUVa
+typealias OKLab = ColorOKLABa
+typealias OKHSV = ColorOKHSVa
+typealias OKHSL = ColorOKHSLa
+typealias OKLCH = ColorOKLCHa
+typealias XSL = ColorXSLa
+typealias XSV = ColorXSVa
+typealias XSLuv = ColorXSLUVa
+typealias CIEXYZ = ColorXYZa
+typealias LCHab = ColorLCHABa
+typealias LCHuv = ColorLCHUVa
