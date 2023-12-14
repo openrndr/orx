578cc57bf91287398e0496935b5cc6e41c136dbe
Edwin Jakobs <edwin@rndr.studio>
2023-12-14 11:03:02 +0100

    [orx-gitarchiver] Disable GitArchiverDemo01.kt in an attempt to fix README generation

--- orx-jvm/orx-git-archiver/src/demo/kotlin/GitArchiverDemo01.kt
+++ orx-jvm/orx-git-archiver/src/demo/kotlin/GitArchiverDemo01.kt
@@ -1,25 +1,25 @@
-
-import org.openrndr.application
-import org.openrndr.extensions.Screenshots
-import org.openrndr.extensions.SingleScreenshot
-import org.openrndr.extra.gitarchiver.GitArchiver
-
-fun main() = application {
-    program {
-        if (System.getProperty("takeScreenshot") == "true") {
-            extend(SingleScreenshot()) {
-                this.outputFile = System.getProperty("screenshotPath")
-            }
-        }
-        val ga = extend(GitArchiver()) {
-            commitOnRun = true
-            commitOnRequestAssets = false
-        }
-        extend(Screenshots())
-        extend {
-
-
-        }
-    }
-
-}
+//
+//import org.openrndr.application
+//import org.openrndr.extensions.Screenshots
+//import org.openrndr.extensions.SingleScreenshot
+//import org.openrndr.extra.gitarchiver.GitArchiver
+//
+//fun main() = application {
+//    program {
+//        if (System.getProperty("takeScreenshot") == "true") {
+//            extend(SingleScreenshot()) {
+//                this.outputFile = System.getProperty("screenshotPath")
+//            }
+//        }
+//        val ga = extend(GitArchiver()) {
+//            commitOnRun = true
+//            commitOnRequestAssets = false
+//        }
+//        extend(Screenshots())
+//        extend {
+//
+//
+//        }
+//    }
+//
+//}
