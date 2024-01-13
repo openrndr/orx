547089bb671c3bf495a16fdb706a54ba0dcfaa23
Edwin Jakobs <edwin@rndr.studio>
2024-01-13 20:07:52 +0100

    [orx-expression-evaluator] Fix task dependencies

--- orx-expression-evaluator/build.gradle.kts
+++ orx-expression-evaluator/build.gradle.kts
@@ -58,3 +58,7 @@ kotlin {
 tasks.withType<KotlinCompile<*>> {
     dependsOn(generateKotlinGrammarSource)
 }
+
+tasks.withType<org.gradle.jvm.tasks.Jar> {
+    dependsOn(generateKotlinGrammarSource)
+}
