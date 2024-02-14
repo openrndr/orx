fc6a19a3c756b02edc1c2e8e25bb782f569af503
arthur <arthurvimond@gmail.com>
2024-02-14 10:17:29 +0100

    [orx-syphon] Add support for arm64 architecture

--- orx-jvm/orx-syphon/src/main/kotlin/jsyphon/JSyphonNative.kt
+++ orx-jvm/orx-syphon/src/main/kotlin/jsyphon/JSyphonNative.kt
@@ -9,8 +9,16 @@ object JSyphonNative {
     init {
         require(Platform.type == PlatformType.MAC) { "orx-syphon only works on macOS, your platform is not supported" }
 
+        val arch = System.getProperty("os.arch")
+        val libraries = when (arch) {
+            "x86-64",
+            "x86_64",
+            "amd64",
+            "x64" -> arrayOf("Syphon-x86", "libJSyphon-x86.jnilib")
+
+            else -> arrayOf("Syphon-arm64", "libJSyphon-arm64.jnilib")
+        }
         val tempBase = Platform.tempDirectory()
-        val libraries = arrayOf("Syphon", "libJSyphon.jnilib")
         val tempDir = File(tempBase, "orx-syphon")
         tempDir.mkdirs()
         for (library in libraries) {
