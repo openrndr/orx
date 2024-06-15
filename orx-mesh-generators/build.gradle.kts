607212214fb323c261c0ba3d78f4ddd766fc0c01
Kazik Pogoda <morisil@xemantic.com>
2024-06-15 13:50:36 +0200

    orx-mesh-generators: add shaders processing to gradle build

--- orx-mesh-generators/build.gradle.kts
+++ orx-mesh-generators/build.gradle.kts
@@ -10,6 +10,7 @@ kotlin {
                 api(libs.openrndr.application)
                 api(libs.openrndr.math)
                 implementation(project(":orx-shapes"))
+                implementation(project(":orx-compute-shaders"))
             }
         }
 
@@ -20,6 +21,7 @@ kotlin {
                 implementation(project(":orx-mesh-generators"))
                 implementation(project(":orx-camera"))
                 implementation(project(":orx-noise"))
+                implementation(project(":orx-point-clouds"))
             }
         }
     }
