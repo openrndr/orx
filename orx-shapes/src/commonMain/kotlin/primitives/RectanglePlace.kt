ee8a709cc0ebc20517e1997acc047fd491109be0
Edwin Jakobs <edwin@rndr.studio>
2025-02-27 10:17:53 +0100

    [orx-shapes] Add `placeIn` methods for Rectangle and Box

--- orx-shapes/src/commonMain/kotlin/primitives/RectanglePlace.kt
+++ orx-shapes/src/commonMain/kotlin/primitives/RectanglePlace.kt
@@ -20,3 +20,19 @@ fun Rectangle.place(item: Rectangle, anchor: Vector2 = Vector2(0.5, 0.5), itemAn
         height = item.height
     )
 }
+
+/**
+ * Positions the current rectangle (`this`) within the given `container` rectangle.
+ * The placement is determined by aligning the `itemAnchor` of the current rectangle to
+ * the `anchor` point within the container rectangle.
+ *
+ * @param container The rectangle within which the current rectangle will be positioned.
+ * @param anchor The relative position of the reference point within the `container` rectangle.
+ *               By default, it is set to `(0.5, 0.5)`, which represents the center.
+ * @param itemAnchor The relative position of the anchor point within the current rectangle.
+ *                   Defaults to the value of `anchor`.
+ * @return A new rectangle representing the current rectangle positioned within the container.
+ */
+fun Rectangle.placeIn(container: Rectangle, anchor: Vector2 = Vector2(0.5, 0.5), itemAnchor: Vector2 = anchor): Rectangle {
+    return container.place(this, anchor, itemAnchor)
+}
