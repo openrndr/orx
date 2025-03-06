e944bd30d36952b330f1411870d1222abc96fce8
Edwin Jakobs <edwin@rndr.studio>
2025-03-06 12:22:48 +0100

    [orx-shapes] Remove `System.setProperty()` add Camera2D to DemoText01.kt

--- orx-shapes/src/jvmDemo/kotlin/text/DemoText01.kt
+++ orx-shapes/src/jvmDemo/kotlin/text/DemoText01.kt
@@ -3,23 +3,23 @@ package text
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.font.loadFace
+import org.openrndr.extra.camera.Camera2D
 import org.openrndr.extra.shapes.bounds.bounds
 import org.openrndr.extra.shapes.text.shapesFromText
 import org.openrndr.internal.Driver
 
 fun main() = application {
-    System.setProperty("org.openrndr.draw.wait_for_finish", "true")
     configure {
         width = 720
         height = 720
     }
     program {
-
         val face =
             loadFace("https://github.com/IBM/plex/raw/master/packages/plex-mono/fonts/complete/otf/IBMPlexMono-Bold.otf")
         val shapes = shapesFromText(face, "SUCH\nVECTOR\nSUCH\nTEXT", 150.0)
 
         val bounds = shapes.bounds
+        extend(Camera2D())
         extend {
             drawer.clear(ColorRGBa.PINK)
             drawer.translate(-bounds.corner)
