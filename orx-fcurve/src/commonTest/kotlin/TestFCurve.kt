a1b6ead866b3f010084d4a1c50c383bfabb50c06
Edwin Jakobs <edwin@rndr.studio>
2025-06-27 07:30:58 +0200

    [orx-fcurve] Workaround for Kotlin 2.2 bug that shows up during testing in javascript mode.
    The bug shows up like this:
    TypeError: fc.value$default_ruhk1_k$ is not a function
    	at TestEFCurve.protoOf.testConstantExpression_4i11z6(orx/orx-fcurve/src/commonTest/kotlin/TestEFCurve.kt:59)
    	at <global>.fn(kotlin/orx-orx-fcurve-test.js:121)
    	at Context.<anonymous>(orx/build/js/node_modules/kotlin-web-helpers/src/KotlinTestTeamCityConsoleAdapter.ts:72)
    	at <global>.processImmediate(node:internal/timers:478)

--- orx-fcurve/src/commonTest/kotlin/TestFCurve.kt
+++ orx-fcurve/src/commonTest/kotlin/TestFCurve.kt
@@ -7,9 +7,9 @@ class TestFCurve {
     fun testConstantExpression() {
         val text = "10.5"
         val fc = fcurve(text)
-        assertEquals(10.5, fc.value(0.0))
-        assertEquals(10.5, fc.value(1.0))
-        assertEquals(10.5, fc.value(-1.0))
+        assertEquals(10.5, fc.value(0.0, null))
+        assertEquals(10.5, fc.value(1.0, null))
+        assertEquals(10.5, fc.value(-1.0, null))
 
         val normalizedSampler = fc.sampler(true)
         assertEquals(10.5, normalizedSampler(0.0))
@@ -22,8 +22,8 @@ class TestFCurve {
         run {
             val text = "H-1 L 5 5"
             val fc = fcurve(text)
-            assertEquals(0.0, fc.value(-1.0))
-            assertEquals(5.0, fc.value(4.0))
+            assertEquals(0.0, fc.value(-1.0, null))
+            assertEquals(5.0, fc.value(4.0, null))
             assertEquals(-1.0, fc.start)
             assertEquals(4.0, fc.end)
             assertEquals(5.0, fc.duration)
@@ -31,8 +31,8 @@ class TestFCurve {
         run {
             val text = "H1 L 5 5"
             val fc = fcurve(text)
-            assertEquals(0.0, fc.value(1.0))
-            assertEquals(5.0, fc.value(6.0))
+            assertEquals(0.0, fc.value(1.0, null))
+            assertEquals(5.0, fc.value(6.0, null))
             assertEquals(1.0, fc.start)
             assertEquals(6.0, fc.end)
             assertEquals(5.0, fc.duration)
