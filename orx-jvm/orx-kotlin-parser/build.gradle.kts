9e803b8a4ed0181095db2754d2d41e0b37cacfc0
Edwin Jakobs <edwin@rndr.studio>
2025-08-01 08:15:01 +0200

    [orx-kotlin-parser] Add dependency for compileTestKotlin

--- orx-jvm/orx-kotlin-parser/build.gradle.kts
+++ orx-jvm/orx-kotlin-parser/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(libs.antlr.runtime)
 }
 
+tasks.getByName("compileTestKotlin").dependsOn("generateTestGrammarSource")
 tasks.getByName("compileKotlin").dependsOn("generateGrammarSource")
 tasks.getByName("sourcesJar").dependsOn("generateGrammarSource")
 tasks.named("dokkaGeneratePublicationHtml") { dependsOn("generateGrammarSource") }
