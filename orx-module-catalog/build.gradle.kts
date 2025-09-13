2979963d3f4a39639ebd9ebe6fed38f0b207de2a
Edwin Jakobs <edwin@rndr.studio>
2025-09-13 15:52:55 +0200

    [orx-module-catalog] Add POM details

--- orx-module-catalog/build.gradle.kts
+++ orx-module-catalog/build.gradle.kts
@@ -80,7 +80,37 @@ group = "org.openrndr.extra"
 publishing {
     publications {
         create<MavenPublication>("maven") {
+            groupId = "org.openrndr.extra"
+            artifactId = "orx-module-catalog"
+            description = "ORX module catalog"
             from(components["versionCatalog"])
+            pom {
+                name.set(project.name)
+                description.set(project.name)
+                url.set("https://openrndr.org")
+                developers {
+                    developer {
+                        id.set("edwinjakobs")
+                        name.set("Edwin Jakobs")
+                        email.set("edwin@openrndr.org")
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("BSD-2-Clause")
+                        url.set("https://github.com/openrndr/orx/blob/master/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git@github.com:openrndr/orx.git")
+                    developerConnection.set("scm:git:ssh://github.com/openrndr/orx.git")
+                    url.set("https://github.com/openrndr/orx")
+                }
+            }
         }
+
     }
 }
