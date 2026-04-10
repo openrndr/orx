b0026ad514e963749d93007b495bc503c9d66b3c
Abe Pazos <abe@hamoid.com>
2026-04-10 10:38:11 +0200

    [orx-shapes] Add radii to RoundedRectangle

--- orx-shapes/src/commonMain/kotlin/primitives/RoundedRectangle.kt
+++ orx-shapes/src/commonMain/kotlin/primitives/RoundedRectangle.kt
@@ -3,26 +3,37 @@ package org.openrndr.extra.shapes.primitives
 import org.openrndr.draw.Drawer
 import org.openrndr.math.Vector2
 import org.openrndr.shape.Rectangle
-import org.openrndr.shape.contour
 import org.openrndr.shape.ShapeContour
+import org.openrndr.shape.contour
 import kotlin.jvm.JvmRecord
 import kotlin.math.min
 
+/**
+ * Rounded rectangle. When `radii` are given (instead of `radius`), they are specified clockwise
+ * starting at the top-left. Fewer than 4 values are accepted and repeated cyclically.
+ * (e.g., providing 2 values alternates them, providing 1 value applies it to all corners).
+ */
 @JvmRecord
-data class RoundedRectangle(val corner: Vector2, val width: Double, val height: Double, val radius: Double) {
-    constructor(x: Double, y: Double, width: Double, height: Double, radius: Double) : this(
-        Vector2(x, y),
-        width,
-        height,
-        radius
-    )
+data class RoundedRectangle(val corner: Vector2, val width: Double, val height: Double, val radii: List<Double>) {
 
-    constructor(rectangle: Rectangle, radius: Double) : this(
-        rectangle.corner,
-        rectangle.width,
-        rectangle.height,
-        radius
-    )
+    init {
+        require(radii.isNotEmpty()) { "RoundedRectangle can't be constructed with zero radii" }
+    }
+
+    constructor(x: Double, y: Double, width: Double, height: Double, radii: List<Double>) :
+            this(Vector2(x, y), width, height, radii)
+
+    constructor(rectangle: Rectangle, radii: List<Double>) :
+            this(rectangle.corner, rectangle.width, rectangle.height, radii)
+
+    constructor(corner: Vector2, width: Double, height: Double, radius: Double) :
+            this(corner, width, height, listOf(radius))
+
+    constructor(x: Double, y: Double, width: Double, height: Double, radius: Double) :
+            this(Vector2(x, y), width, height, listOf(radius))
+
+    constructor(rectangle: Rectangle, radius: Double) :
+            this(rectangle.corner, rectangle.width, rectangle.height, listOf(radius))
 
     /** the center of the rounded rectangle */
     val center: Vector2
@@ -35,35 +46,46 @@ data class RoundedRectangle(val corner: Vector2, val width: Double, val height: 
     val contour
         get() = contour {
             // A higher radius than half the width/height makes it go weird
-            val r = min(min(radius, width / 2), height / 2)
+            val r = List(4) {
+                min(min(radii[it % radii.size], width / 2), height / 2)
+            }
 
-            moveTo(x + r, y)
-            lineTo(x + width - r, y)
+            moveTo(x + r[0], y)
+            lineTo(x + width - r[1], y)
 
-            arcTo(r, r, 90.0, false, true, Vector2(x + width, y + r))
-            lineTo(x + width, y + height - r)
+            arcTo(r[1], r[1], 90.0, false, true, Vector2(x + width, y + r[1]))
+            lineTo(x + width, y + height - r[2])
 
-            arcTo(r, r, 90.0, false, true, Vector2(x + width - r, y + height))
-            lineTo(x + r, y + height)
+            arcTo(r[2], r[2], 90.0, false, true, Vector2(x + width - r[2], y + height))
+            lineTo(x + r[3], y + height)
 
-            arcTo(r, r, 90.0, false, true, Vector2(x, y + height - r))
-            lineTo(x, y + r)
+            arcTo(r[3], r[3], 90.0, false, true, Vector2(x, y + height - r[3]))
+            lineTo(x, y + r[0])
 
-            arcTo(r, r, 90.0, false, true, Vector2(x + r, y))
-            close()
-        }
+            arcTo(r[0], r[0], 90.0, false, true, Vector2(x + r[0], y))
+            close()        }
 
     val shape
         get() = contour.shape
 }
 
-fun Drawer.roundedRectangle(x: Double, y: Double, width: Double, height: Double, radius: Double) =
-    contour(RoundedRectangle(x, y, width, height, radius).contour)
-
-fun Drawer.roundedRectangle(position: Vector2, width: Double, height: Double, radius: Double) =
-    contour(RoundedRectangle(position, width, height, radius).contour)
-
 fun Drawer.roundedRectangle(roundedRectangle: RoundedRectangle) =
     contour(roundedRectangle.contour)
 
-fun Rectangle.toRounded(radius: Double) = RoundedRectangle(this, radius)
+// radii
+fun Drawer.roundedRectangle(x: Double, y: Double, width: Double, height: Double, radii: List<Double>) =
+    contour(RoundedRectangle(x, y, width, height, radii).contour)
+
+fun Drawer.roundedRectangle(position: Vector2, width: Double, height: Double, radii: List<Double>) =
+    contour(RoundedRectangle(position, width, height, radii).contour)
+
+fun Rectangle.toRounded(radii: List<Double>) = RoundedRectangle(this, radii)
+
+// radius
+fun Drawer.roundedRectangle(x: Double, y: Double, width: Double, height: Double, radius: Double) =
+    contour(RoundedRectangle(x, y, width, height, listOf(radius)).contour)
+
+fun Drawer.roundedRectangle(position: Vector2, width: Double, height: Double, radius: Double) =
+    contour(RoundedRectangle(position, width, height, listOf(radius)).contour)
+
+fun Rectangle.toRounded(radius: Double) = RoundedRectangle(this, listOf(radius))
