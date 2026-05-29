1741ef64845675129aa4947d374197f097d7011c
Edwin Jakobs <edwin@rndr.studio>
2026-05-30 00:12:53 +0200

    Add demo for RtreeDcelEdge2D

---
+++ orx-mesh/src/jvmDemo/kotlin/dcel/DemoRtreeEdges01.kt
@@ -0,0 +1,41 @@
+package dcel
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.extra.mesh.dcel.convert.toDcel
+import org.openrndr.extra.mesh.dcel.navigate.allEdges
+import org.openrndr.extra.mesh.dcel.query.verticesForEdge
+import org.openrndr.extra.mesh.generate.gridMesh
+import org.openrndr.extra.mesh.rtree.RtreeDcelEdge2D
+
+fun main() {
+    application {
+        configure {
+            width = 720
+            height = 720
+        }
+        program {
+            val grid = gridMesh(drawer.bounds, 10, 10)
+            val dcel = grid.toDcel()
+            val rtree = RtreeDcelEdge2D(dcel)
+            dcel.allEdges().forEach {
+                rtree.insert(it)
+            }
+            extend {
+                drawer.clear(ColorRGBa.PINK)
+                dcel.allEdges().map {
+                    val vertices = dcel.verticesForEdge(it)
+                    drawer.lineSegment(dcel.vertices[vertices[0]].position, dcel.vertices[vertices[1]].position)
+                }
+                drawer.stroke = ColorRGBa.WHITE
+                val edges = rtree.findKNearest(mouse.position, 3)
+                edges.forEach {
+                    val vertices = dcel.verticesForEdge(it)
+                    drawer.lineSegment(dcel.vertices[vertices[0]].position, dcel.vertices[vertices[1]].position)
+                }
+
+            }
+
+        }
+    }
+}
