openrndr/orx

commit 9950cba86c85bf7f88554de7894be8c475ebb5ca

[orx-panel] Add Document api

orx-jvm/orx-panel/src/main/kotlin/org/openrndr/panel/ControlManager.kt
@@ -7,6 +7,7 @@ import org.openrndr.draw.*
 import org.openrndr.internal.Driver
 import org.openrndr.math.Matrix44
 import org.openrndr.math.Vector2
+import org.openrndr.panel.document.Document
 import org.openrndr.panel.elements.*
 import org.openrndr.panel.layout.Layouter
 import org.openrndr.panel.style.*
@@ -20,8 +21,19 @@ import kotlin.contracts.contract
 private val logger = KotlinLogging.logger {}
 
 class ControlManager : Extension {
+    var document: Document? = null
+        set(value) {
+            if (field !== value) {
+                field = value
+                body = value?.body
+                layouter = value?.layouter
+                value?.body?.controlManager = this
+            }
+        }
+
     var body: Element? = null
-    val layouter = Layouter()
+    var layouter: Layouter? = null
+
     val fontManager = FontManager()
     lateinit var window: Window
     private val renderTargetCache = HashMap<Element, RenderTarget>()
@@ -34,7 +46,7 @@ class ControlManager : Extension {
 
     init {
         fontManager.register("default", resourceUrl("/fonts/Roboto-Regular.ttf"))
-        layouter.styleSheets.addAll(defaultStyles().flatMap { it.flatten() })
+//        layouter.styleSheets.addAll(defaultStyles().flatMap { it.flatten() })
     }
 
     class DropInput {
@@ -51,7 +63,7 @@ class ControlManager : Extension {
         private var lastTarget: Element? = null
         var target: Element? = null
             set(value) {
-                if (value != field) {
+                if (value !== field) {
                     field?.pseudoClasses?.remove(ElementPseudoClass("active"))
                     field?.keyboard?.focusLost?.trigger(FocusEvent())
                     value?.keyboard?.focusGained?.trigger(FocusEvent())
@@ -435,8 +447,7 @@ class ControlManager : Extension {
 
                     body?.let {
                         program.drawer.clear(ColorRGBa.BLACK.opacify(0.0))
-                        layouter.computeStyles(it)
-                        layouter.layout(it)
+                        document?.layout()
                         drawElement(it, program.drawer, 0, 0)
                         drawElement(it, program.drawer, 1, 0)
                         drawElement(it, program.drawer, 1000, 0)
@@ -458,15 +469,29 @@ class ControlManager : Extension {
     }
 }
 
+private fun ControlManager.existingOrNewDocumentWithDefaultStyleSheets(): Document {
+    return if (document == null) {
+        val document = Document()
+        document.styleSheets.addAll(defaultStyles())
+        document
+    } else {
+        document!!
+    }
+}
+
 class ControlManagerBuilder(val controlManager: ControlManager) {
     fun styleSheet(selector: CompoundSelector, init: StyleSheet.() -> Unit): StyleSheet {
+        val document = controlManager.existingOrNewDocumentWithDefaultStyleSheets()
         val styleSheet = StyleSheet(selector).apply { init() }
-        controlManager.layouter.styleSheets.addAll(styleSheet.flatten())
+        document.styleSheets.add(styleSheet)
+        controlManager.document = document
         return styleSheet
     }
 
     fun styleSheets(styleSheets: List<StyleSheet>) {
-        controlManager.layouter.styleSheets.addAll(styleSheets.flatMap { it.flatten() })
+        val document = controlManager.existingOrNewDocumentWithDefaultStyleSheets()
+        controlManager.layouter?.styleSheets?.addAll(styleSheets)
+        controlManager.document = document
     }
 
     @OptIn(ExperimentalContracts::class)
@@ -474,27 +499,33 @@ class ControlManagerBuilder(val controlManager: ControlManager) {
         contract {
             callsInPlace(init, InvocationKind.EXACTLY_ONCE)
         }
-        val body = Body(controlManager)
-        body.init()
-        controlManager.body = body
+
+        val document = controlManager.existingOrNewDocumentWithDefaultStyleSheets()
+        document.body.init()
+
+        controlManager.document = document
     }
 }
 
 
 fun ControlManager.styleSheet(selector: CompoundSelector, init: StyleSheet.() -> Unit): StyleSheet {
+    val document = existingOrNewDocumentWithDefaultStyleSheets()
     val styleSheet = StyleSheet(selector).apply { init() }
-    layouter.styleSheets.addAll(styleSheet.flatten())
+    document.styleSheets.add(styleSheet)
+    this.document = document
     return styleSheet
 }
 
 fun ControlManager.styleSheets(styleSheets: List<StyleSheet>) {
-    layouter.styleSheets.addAll(styleSheets.flatMap { it.flatten() })
+    val document = existingOrNewDocumentWithDefaultStyleSheets()
+    document.styleSheets.addAll(styleSheets)
+    this.document = document
 }
 
 fun ControlManager.layout(init: Body.() -> Unit) {
-    val body = Body(this)
-    body.init()
-    this.body = body
+    val document = existingOrNewDocumentWithDefaultStyleSheets()
+    document.body.init()
+    this.document = document
 }
 
 @OptIn(ExperimentalContracts::class)
@@ -508,7 +539,7 @@ fun Program.controlManager(
     val cm = ControlManager()
     cm.program = this
     cm.fontManager.register("default", resourceUrl("/fonts/Roboto-Regular.ttf"))
-    cm.layouter.styleSheets.addAll(defaultStyles.flatMap { it.flatten() })
+    cm.layouter?.styleSheets?.addAll(defaultStyles.flatMap { it.flatten() })
     val cmb = ControlManagerBuilder(cm)
     cmb.builder()
     return cm

orx-jvm/orx-panel/src/demo/kotlin/DemoToolWindows01.kt
@@ -1,0 +1,128 @@
+import org.openrndr.Hit
+import org.openrndr.WindowConfiguration
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.math.Vector2
+import org.openrndr.panel.ControlManager
+import org.openrndr.panel.document.body
+import org.openrndr.panel.document.document
+import org.openrndr.panel.elements.button
+import org.openrndr.panel.elements.div
+import org.openrndr.panel.elements.h1
+import org.openrndr.panel.elements.h3
+import org.openrndr.panel.elements.slider
+import org.openrndr.panel.elements.style
+import org.openrndr.panel.style.Display
+import org.openrndr.panel.style.FlexDirection
+import org.openrndr.panel.style.background
+import org.openrndr.panel.style.color
+import org.openrndr.panel.style.columnGap
+import org.openrndr.panel.style.defaultStyles
+import org.openrndr.panel.style.display
+import org.openrndr.panel.style.flexDirection
+import org.openrndr.panel.style.height
+import org.openrndr.panel.style.length
+import org.openrndr.panel.style.padding
+import org.openrndr.panel.style.percent
+import org.openrndr.panel.style.textAlign
+import org.openrndr.panel.style.textHorizontalAlign
+import org.openrndr.panel.style.textVerticalAlign
+import org.openrndr.panel.style.width
+import org.openrndr.shape.Rectangle
+import org.openrndr.window
+
+fun main() = application {
+    program {
+
+        fun createToolWindow() = window(
+            WindowConfiguration(
+                width = 200,
+                height = 500,
+                hideDecorations = true,
+                resizable = true,
+                utilityWindow = true
+            )
+        ) {
+            val toolWindow = this.window
+            toolWindow.hitTest = { v: Vector2 ->
+                if (v in Rectangle(
+                        0.0,
+                        0.0,
+                        200.0,
+                        24.0
+                    )
+                ) Hit.DRAG else Hit.NORMAL
+            }
+            val document = document {
+                styleSheets.addAll(defaultStyles())
+                body {
+                    div {
+                        style {
+                            width = length { 100.percent }
+                            height = length { 100.percent }
+                            display = Display.FLEX
+                            flexDirection = FlexDirection.Column
+                            columnGap = length { 5 }
+                            background = color { ColorRGBa.GRAY.shade(0.9) }
+                        }
+
+                        div {
+                            style {
+                                this.width = length { 100.percent }
+                                this.height = length { 24 }
+                                background = color { ColorRGBa.GRAY }
+                            }
+                            h3 {
+                                style {
+                                    textVerticalAlign = textAlign { center }
+                                    textHorizontalAlign = textAlign { center }
+                                }
+                                "Foolbar"
+                            }
+                        }
+                        for (i in 0 until 7) {
+                            slider {
+                                label = "Slider $i"
+                            }
+                        }
+                        button {
+                            label = "Close"
+                            events.clicked.listen {
+                                toolWindow.close()
+                            }
+                        }
+                    }
+                }
+            }
+            val cm = ControlManager()
+            cm.document = document
+            extend(cm)
+        }
+
+        val document = document {
+            styleSheets.addAll(defaultStyles())
+            body {
+                div {
+                    style {
+                        this.width = length { 100.percent }
+                        this.height = length { 100.percent }
+                        padding(length { 20.px })
+                    }
+                    h1 {
+                        "Hello world!"
+                    }
+                    button {
+                        label = "Create tool window"
+                        events.clicked.listen {
+                            createToolWindow()
+                        }
+                    }
+                }
+            }
+        }
+        val cm = ControlManager()
+        cm.document = document
+
+        extend(cm)
+    }
+}

orx-jvm/orx-panel/src/main/kotlin/org/openrndr/panel/collections/ObservableArrayList.kt
@@ -1,0 +1,71 @@
+package org.openrndr.panel.collections
+
+import org.openrndr.events.Event
+import java.util.concurrent.CopyOnWriteArrayList
+
+/**
+ * A specialized implementation of ArrayList that triggers an event whenever the list is modified.
+ * This class supports observing additions, removals, and clear operations, and notifies listeners
+ * via the `changed` event.
+ *
+ * @param E the type of elements in this list
+ */
+class ObservableArrayList<E> : ArrayList<E>(), AutoCloseable {
+
+    val changed = Event<ObservableArrayList<E>>()
+
+    override fun addAll(elements: Collection<E>): Boolean {
+        return if (super.addAll(elements)) {
+            changed.trigger(this)
+            true
+        } else {
+            false
+        }
+    }
+
+    override fun add(element: E): Boolean {
+        return if (super.add(element)) {
+            changed.trigger(this)
+            true
+        } else {
+            false
+        }
+    }
+
+    override fun remove(element: E): Boolean {
+        return if (super.remove(element)) {
+            changed.trigger(this)
+            true
+        } else {
+            false
+        }
+    }
+
+    override fun removeAll(elements: Collection<E>): Boolean {
+        return if (super.removeAll(elements)) {
+            changed.trigger(this)
+            true
+        } else {
+            false
+        }
+    }
+
+    override fun retainAll(elements: Collection<E>): Boolean {
+        return if (super.retainAll(elements)) {
+            changed.trigger(this)
+            true
+        } else {
+            false
+        }
+    }
+    override fun clear() {
+        super.clear()
+        changed.trigger(this)
+    }
+
+    override fun close() {
+        // prevent emitting change events while closing
+        super.clear()
+        changed.close()
+    }
+}

orx-jvm/orx-panel/src/main/kotlin/org/openrndr/panel/document/Document.kt
@@ -1,0 +1,54 @@
+package org.openrndr.panel.document
+
+import org.openrndr.panel.ControlManager
+import org.openrndr.panel.collections.ObservableArrayList
+import org.openrndr.panel.elements.Body
+import org.openrndr.panel.layout.Layouter
+import org.openrndr.panel.style.StyleSheet
+import org.openrndr.panel.style.flatten
+
+/**
+ * Represents a document with a hierarchical structure of elements,
+ * styles, and layout capabilities. The `Document` class manages
+ * the layout and styling system by utilizing a `Layouter` and an
+ * observable list of stylesheets.
+ *
+ * Features include:
+ * - The ability to manage styles through a list of `StyleSheet` objects.
+ * - Automatic tracking of changes to stylesheets to ensure layout updates are performed when necessary.
+ * - Support for laying out the document body based on computed styles from the provided stylesheets.
+ * - A controllable lifecycle to clean up resources when the document is no longer in use.
+ *
+ * Implements [AutoCloseable] to ensure that resources such as `controlManager`,
+ * `styleSheets`, and `body` are cleaned up appropriately.
+ */
+class Document : AutoCloseable {
+    var controlManager: ControlManager? = null
+    val styleSheets = ObservableArrayList<StyleSheet>()
+    var body: Body = Body(null)
+    val layouter = Layouter()
+
+    init {
+        styleSheets.changed.listen {
+            dirtySheets = true
+        }
+    }
+    private var dirtySheets = true
+
+    fun layout() {
+        if (dirtySheets) {
+            layouter.styleSheets.clear()
+            layouter.styleSheets.addAll(styleSheets.flatMap { it.flatten() })
+            dirtySheets = false
+        }
+
+        layouter.computeStyles(body)
+        layouter.layout(body)
+    }
+
+    override fun close() {
+        controlManager = null
+        styleSheets.close()
+        body.close()
+    }
+}

orx-jvm/orx-panel/src/main/kotlin/org/openrndr/panel/document/DocumentExtensions.kt
@@ -1,0 +1,7 @@
+package org.openrndr.panel.document
+
+import org.openrndr.panel.elements.Body
+
+fun document(init: Document.() -> Unit) = Document().apply(init)
+
+fun Document.body(init: Body.() -> Unit) = body.apply(init)

orx-jvm/orx-panel/src/main/kotlin/org/openrndr/panel/elements/Body.kt
@@ -2,4 +2,4 @@ package org.openrndr.panel.elements
 
 import org.openrndr.panel.ControlManager
 
-class Body(val controlManager: ControlManager) : Element(ElementType("Body"))
+class Body(var controlManager: ControlManager?) : Element(ElementType("Body"))

orx-jvm/orx-panel/src/main/kotlin/org/openrndr/panel/elements/Colorpicker.kt
@@ -93,7 +93,7 @@ class Colorpicker : Element {
 
             if (KeyModifier.CTRL in it.modifiers || KeyModifier.SUPER in it.modifiers) {
                 if (it.name == "v") {
-                    (root() as Body).controlManager.program.clipboard.contents?.let {
+                    (root() as Body).controlManager?.program?.clipboard?.contents?.let {
                         keyboardInput += it
                         draw.dirty = true
                     }

orx-jvm/orx-panel/src/main/kotlin/org/openrndr/panel/elements/ColorpickerButton.kt
@@ -125,7 +125,7 @@ class ColorpickerButton : Element(ElementType("colorpicker-button")) {
         }
 
         override fun draw(drawer: Drawer) {
-            (root() as Body).controlManager.keyboardInput.requestFocus(children[0])
+            (root() as Body).controlManager?.keyboardInput?.requestFocus(children[0])
             drawer.fill = ((computedStyle.background as? Color.RGBa)?.color ?: ColorRGBa.PINK)
             drawer.rectangle(0.0, 0.0, screenArea.width, screenArea.height)
         }

commit 167d0ad939ddee60946c283ded64fedb7b84ca53

[orx-panel] Make use of user-provided styleSheets in GUI
Fixes issue introduced in 9950cba86c85bf7f88554de7894be8c475ebb5ca

orx-jvm/orx-panel/src/main/kotlin/org/openrndr/panel/ControlManager.kt
@@ -506,7 +506,7 @@ class ControlManagerBuilder(val controlManager: ControlManager) {
     fun styleSheets(styleSheets: List<StyleSheet>) {
         val document = controlManager.existingOrNewDocumentWithDefaultStyleSheets()
         controlManager.document = document
-        controlManager.layouter?.styleSheets?.addAll(styleSheets)
+        document.styleSheets.addAll(styleSheets)
     }
 
     @OptIn(ExperimentalContracts::class)
@@ -553,7 +553,9 @@ fun Program.controlManager(
     val cm = ControlManager()
     cm.program = this
     cm.fontManager.register("default", resourceUrl("/fonts/Roboto-Regular.ttf"))
-    cm.layouter?.styleSheets?.addAll(defaultStyles.flatMap { it.flatten() })
+    val document = Document()
+    document.styleSheets.addAll(defaultStyles)
+    cm.document = document
     val cmb = ControlManagerBuilder(cm)
     cmb.builder()
     return cm